fa363fd6036ae80fab3d36bb93850e4b0e454b6a
msjang <admin@codemaru.co.kr>
2026-06-11 00:19:51 +0900

    Improve library quality and safety

--- Interfaces/IBehavior.cs
+++ Interfaces/IBehavior.cs
@@ -8,17 +8,7 @@ namespace Dreamine.MVVM.Behaviors.Core.Interfaces
 	/// - View 요소(DependencyObject)에 연결/분리되는 확장 동작을 정의합니다.
 	/// - XAML 또는 코드에서 동적으로 attach 가능한 구조 기반입니다.
 	/// </summary>
-	public interface IBehavior
+	public interface IBehavior : IAttachedObject
 	{
-		/// <summary>
-		/// 지정된 UI 요소에 이 Behavior를 연결합니다.
-		/// </summary>
-		/// <param name="dependencyObject">연결 대상 요소</param>
-		void Attach(DependencyObject dependencyObject);
-
-		/// <summary>
-		/// 현재 연결된 UI 요소에서 이 Behavior를 분리합니다.
-		/// </summary>
-		void Detach();
 	}
 }
